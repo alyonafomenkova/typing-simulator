9074f256617f9a6f9de61982a96510511a0c10ab
Alena Fomenkova <fruitella30sep@mail.ru>
2021-02-12 21:47:49 +0300

    Adding container.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
+import TestingContainer from './components/TestingContainer/TestingContainer';
+import './styles/global.scss';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TestingContainer />
     </div>
   );
 }
