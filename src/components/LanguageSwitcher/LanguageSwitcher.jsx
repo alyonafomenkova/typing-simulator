c2f45cea0d70ab144a15055a5e5b2756129584e3
Alena Fomenkova <fruitella30sep@mail.ru>
2021-02-12 17:22:53 +0300

    Adding LanguageSwitcher component.

---
+++ src/components/LanguageSwitcher/LanguageSwitcher.jsx
@@ -0,0 +1,64 @@
+import Select from 'react-select';
+import keyboard from './images/keyboard-icon.png';
+
+const LanguageSwitcher = () => {
+  const options = [
+    { value: 'Русский', label: 'Русский' },
+    { value: 'English', label: 'English' },
+  ];
+  const customStyles = {
+    container: () => ({
+      minWidth: '150px',
+      position: 'relative',
+    }),
+    menu: () => ({
+      minWidth: '150px',
+      position: 'absolute',
+      top: '38px',
+      left: '0',
+    }),
+    singleValue: () => ({
+      marginLeft: '30px',
+    }),
+    control: (styles) => ({
+      ...styles,
+      minWidth: '150px',
+      borderColor: '#993366',
+      boxShadow: '0 0 0 1px #993366',
+      backgroundImage: `url(${keyboard})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: '8px center',
+      cursor: 'pointer',
+      ':hover': {
+        borderColor: '#993366',
+        boxShadow: '0 0 0 1px #993366',
+        outlineColor: '993366',
+      },
+      ':focus': {
+        borderColor: '#993366',
+        boxShadow: '0 0 0 1px #993366',
+        outlineColor: '993366',
+      },
+    }),
+    option: (styles, { isSelected }) => {
+      return {
+        ...styles,
+        backgroundColor: isSelected ? '#993366' : '#ffccff',
+        color: isSelected ? 'white' : 'black',
+        cursor: 'pointer',
+        textAlign: 'center',
+
+        ':active': {
+          ...styles[':active'],
+          backgroundColor: '#993366',
+          color: 'white',
+        },
+      };
+    },
+  };
+  return (
+    <Select options={options} styles={customStyles} defaultValue={options[0]} />
+  );
+};
+
+export default LanguageSwitcher;
