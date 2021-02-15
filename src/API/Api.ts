bd18fee85691ce288554b06b9053afe77a4c9211
Alena Fomenkova <fruitella30sep@mail.ru>
2021-02-15 17:42:27 +0300

    Adding text loading for different languages.

---
+++ src/API/Api.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+async function getRussianText(sentenceCount: number) {
+  const response = await axios.get(
+    `https://fish-text.ru/get?number=${sentenceCount}`
+  );
+  return response.data.text;
+}
+
+async function getEnglishText(sentenceCount: number) {
+  const response = await axios.get(
+    `https://baconipsum.com/api/?type=all-meat&sentences=${sentenceCount}&start-with-lorem=1`
+  );
+  return response.data;
+}
+
+const getText = (language: string, sentenceCount: number = 5) => {
+  switch (language) {
+    case 'en':
+      return getEnglishText(sentenceCount);
+    default:
+      return getRussianText(sentenceCount);
+  }
+};
+
+export default getText;
--- src/components/Testing/Testing.jsx
+++ src/components/Testing/Testing.jsx
@@ -1,15 +1,27 @@
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import Statistics from '../Statistics/Statistics';
 import styles from './Testing.module.scss';
+import getText from '../../API/Api';
 
-const Testing = () => (
-  <div className={styles.testing}>
-    <div className={styles.mainNext}>
-      testingtestingtesting testingtestingtestingtestingtestingtestingtestingte
-      testingtestingtesting
-      stingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting
+const Testing = () => {
+  const language = useSelector((state) => {
+    return state.testingReducer.language;
+  });
+  const [text, setText] = useState();
+
+  useEffect(() => {
+    getText(language).then((response) => {
+      setText(response);
+    });
+  }, [language]);
+
+  return (
+    <div className={styles.testing}>
+      <div className={styles.mainNext}>{text}</div>
+      <Statistics speed={0} precision={100} />
     </div>
-    <Statistics speed={0} precision={100} />
-  </div>
-);
+  );
+};
 
 export default Testing;
