82f264144d619c521da5ff3fd5e16675bd9aed0b
Alena Fomenkova <fruitella30sep@mail.ru>
2021-02-17 19:30:04 +0300

    Changing rounding.

--- src/helpers/calculatePrecision.ts
+++ src/helpers/calculatePrecision.ts
@@ -2,7 +2,7 @@ const calculatePrecision = (text: string, mistakesCount: number) => {
   const { length } = text;
   const coincidencesNumber = length - mistakesCount;
   const precision = (100 * coincidencesNumber) / length;
-  return Math.round(precision);
+  return Math.floor(precision);
 };
 
 export default calculatePrecision;
